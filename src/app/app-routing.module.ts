4a6d135ea0b8b5db3ef7852d85683738cb6b7264
christophe <christophegueroult@gmail.com>
2021-02-23 17:12:52 +0100

    init config router and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
+import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
 
-const routes: Routes = [];
+// angular.io/
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: PageSignInComponent },
+  { path: 'signup', component: PageSignUpComponent },
+  { path: 'reset', component: PageResetPasswordComponent },
+  { path: 'forgot', component: PageForgotPasswordComponent },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    // Use a custom replacer to display function names in the route configs
+    const replacer = (key: any, value: any) =>
+      typeof value === 'function' ? value.name : value;
+
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
--- src/app/icons/components/icon-cross/icon-cross.component.ts
+++ src/app/icons/components/icon-cross/icon-cross.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
+/**
+ * This component is stupid to generate an icon width fontawesome
+ */
 @Component({
   selector: 'app-icon-cross',
   templateUrl: './icon-cross.component.html',
   styleUrls: ['./icon-cross.component.scss'],
 })
 export class IconCrossComponent implements OnInit {
+  /**
+   * property to use in html for icon binding
+   */
   public myIcon = faTimes;
   constructor() {}
 
+  /**
+   * @function
+   * this function is a life cycle called at the initialization of this class
+   *
+   */
   ngOnInit(): void {}
 }
