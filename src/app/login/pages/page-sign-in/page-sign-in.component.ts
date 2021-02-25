a33da8e685ce1dd500f382d6546109cda8bf7543
christophe <christophegueroult@gmail.com>
2021-02-25 14:35:11 +0100

    exemple observable perso and  unsubscribe

--- src/app/login/pages/page-sign-in/page-sign-in.component.ts
+++ src/app/login/pages/page-sign-in/page-sign-in.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-sign-in',
   templateUrl: './page-sign-in.component.html',
-  styleUrls: ['./page-sign-in.component.scss']
+  styleUrls: ['./page-sign-in.component.scss'],
 })
-export class PageSignInComponent implements OnInit {
+export class PageSignInComponent implements OnInit, OnDestroy {
+  private users = [
+    {
+      name: 'tonton',
+      login: 'user',
+      pass: 'user1',
+    },
+    {
+      name: 'cricri',
+      login: 'user2',
+      pass: 'user2',
+    },
+  ];
+  public obs$: Observable<any>;
+  private sub!: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.obs$ = new Observable((listXSubscribes) => {
+      listXSubscribes.next(this.users);
+    });
   }
 
+  ngOnInit(): void {
+    this.sub = this.obs$.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
