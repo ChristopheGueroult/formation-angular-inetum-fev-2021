747ff279a671646ababb7a2b13f1e232b21d3450
christophe <christophegueroult@gmail.com>
2021-02-26 13:05:25 +0100

    Output() and EventEmitter() and ReactiveForms

--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/core/models/order';
+import { OrdersService } from 'src/app/core/services/orders.service';
 
 @Component({
   selector: 'app-page-add-order',
   templateUrl: './page-add-order.component.html',
-  styleUrls: ['./page-add-order.component.scss']
+  styleUrls: ['./page-add-order.component.scss'],
 })
 export class PageAddOrderComponent implements OnInit {
+  public item = new Order();
+  constructor(private ordersService: OrdersService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public add(item: Order): void {
+    // console.log(item);
+    this.ordersService.add(item).subscribe((res) => {
+      this.router.navigate(['orders']);
+    });
   }
-
 }
